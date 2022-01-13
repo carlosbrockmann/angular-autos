557b7afbbe27e45c526a396b7b57125dc7a783ad
carlosbrockmann <cjbrockmann@topmail-files.de>
2022-01-13 13:33:30 +0100

    3 files modified

--- src/app/sites/autos/autodetails/autodetails.component.ts
+++ src/app/sites/autos/autodetails/autodetails.component.ts
@@ -13,6 +13,8 @@ export class AutodetailsComponent implements OnInit {
   auto: CARS;
   viewstate: string = 'details';
   editmodus: boolean = false;
+  message: string = '';
+  errorMessage: string = '';
 
   private selectedId: number = 0;
 
@@ -22,15 +24,12 @@ export class AutodetailsComponent implements OnInit {
 
     var autos = GlobalConstants.CarList.filter((c) => c.ID == this.selectedId);
     if (autos.length > 0) this.auto = autos[0];
+    if (autos.length == 0) {
+      this.auto = {} as CARS;
+    }
 
     let view = this.route.snapshot.queryParamMap.get('viewstate') || 'details';
-    if (view == 'edit') {
-      this.viewstate = 'edit';
-      this.editmodus = true;
-    } else {
-      this.viewstate = 'details';
-      this.editmodus = false;
-    }
+    this.selectAction(view);
   }
 
   toggleViewState(): void {
@@ -40,6 +39,45 @@ export class AutodetailsComponent implements OnInit {
     } else {
       this.viewstate = 'edit';
       this.editmodus = true;
+    }
+  }
+
+  selectAction(action: string) {
+    this.message = '';
+    this.errorMessage = '';
+    if (!action) action = 'details';
+    if (action == 'details') {
+      this.viewstate = 'details';
+      this.editmodus = false;
+    }
+    if (action == 'edit') {
+      this.viewstate = 'edit';
+      this.editmodus = true;
+    }
+    if (action == 'new') {
+      this.viewstate = 'new';
+      this.editmodus = true;
+      this.auto = {} as CARS;
+      this.auto.ID = GlobalConstants.CarList.length + 1;
+    }
+    if (action == 'save') {
+      var isNew: boolean = false;
+      var tmp = GlobalConstants.CarList.filter((c) => c.ID == this.auto.ID);
+      if (this.auto.Marke) {
+        if (tmp.length == 0) {
+          GlobalConstants.CarList.push(this.auto);
+          this.viewstate = 'details';
+          this.editmodus = false;
+          this.message = 'Datensatz hinzugefügt!';
+        }
+        if (tmp.length > 0) {
+          this.viewstate = 'details';
+          this.editmodus = false;
+          this.message = 'Datensatz gespeichert!';
+        }
+      } else {
+        this.errorMessage = 'Bitte Daten vervollstädigen!';
+      }
     }
   }
 }
